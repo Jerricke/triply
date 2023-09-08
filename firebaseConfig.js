2114e52a4c7e47b281b3060f93e1edc77825f2ce
Jerricke <eejerrickw@gmail.com>
2023-09-08 10:36:53 -0600

    fixed auth

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp, getReactNativePersistence } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
-import { getAuth } from "firebase/auth";
+import { getAuth, getReactNativePersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
@@ -17,5 +17,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const FBDB = getFirestore(app)
-export const FBAUTH = getAuth(app)
+export const FBAUTH = getAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
 
