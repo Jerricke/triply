bbf7f9f712e0d6394f28c4e61c7946f91562cf2c
Jerricke <eejerrickw@gmail.com>
2023-09-08 18:59:46 -0600

    stylized navigation tabs

--- app/logged-in/_layout.jsx
+++ app/logged-in/_layout.jsx
@@ -1,11 +1,13 @@
 import { Tabs } from "expo-router";
+import { Octicons,FontAwesome5 } from '@expo/vector-icons'; 
+import { COLORS } from "../../constants/theme";
 
 export default function LoggedInLayout () {
     return (
-        <Tabs screenOptions={{headerShown: false}}>
-            <Tabs.Screen name="home" />
-            <Tabs.Screen name="startATrip" />
-            <Tabs.Screen name="currentUserProfile" />
+        <Tabs screenOptions={{headerShown: false, tabBarActiveTintColor: COLORS.fg2}} >
+            <Tabs.Screen name="home" options={{tabBarShowLabel: false, tabBarIcon: ({color}) => (<Octicons name="people" size={28} color={color} />)}}/>
+            <Tabs.Screen name="startATrip" options={{tabBarShowLabel: false, tabBarIcon: ({color}) => (<FontAwesome5 name="route" size={28} color={color} />)}}/>
+            <Tabs.Screen name="currentUserProfile" options={{tabBarShowLabel: false, tabBarIcon: ({color}) => (<Octicons name="person" size={28} color={color} />)}}/>
         </Tabs>
     )
 }
