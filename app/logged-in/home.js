0e84d91f74d51639739b1b18155809d87ee67e70
Jerricke <eejerrickw@gmail.com>
2023-09-11 16:06:48 -0600

    created new post component

--- app/logged-in/home.js
+++ app/logged-in/home.js
@@ -4,6 +4,8 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import { FBDB } from '../../firebaseConfig'
 import { collection, getDocs } from "firebase/firestore";
 import PostCard from '../../components/logged-in/PostCard';
+import NewPost from '../../components/logged-in/newPost';
+import { COLORS } from '../../constants/theme';
 
 
 const home = () => {
@@ -28,19 +30,19 @@ const home = () => {
         }
     }
     return (
-        <SafeAreaView style={{flex: 1}}>
-            <View style={{flex: 1, borderColor: "black", borderWidth: 2}}>
+        <SafeAreaView style={{backgroundColor: COLORS.bg1,}}>
+            <View style={{height: "10%", borderColor: "black", borderWidth: 2}}>
                 <Text>test Navigation box</Text>
             </View>
-            <View style={{flex: 9}}>
+            <View style={{height: "75%"}}>
                 <FlatList 
                     data={displayData}
                     renderItem={ ({item}) => <PostCard post={item}/>}
                     keyExtractor={ post => post.id}
                 />
             </View>
-            <View style={{flex: 2, borderColor: "black", borderWidth: 2}}>
-                <Text>test new post box</Text>
+            <View style={{height: "15%"}}>
+                <NewPost />
             </View>
         </SafeAreaView>
     )
--- components/logged-in/newPost.jsx
+++ components/logged-in/newPost.jsx
@@ -1,14 +1,56 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import { COLORS } from '../../constants/theme'
+import { TextInput, TouchableOpacity } from 'react-native-gesture-handler'
 
-const newPost = () => {
-  return (
-    <View>
-      <Text>newPost</Text>
-    </View>
-  )
+const NewPost = () => {
+    const [post, setPost] = useState("")
+
+    const handleCreatePost = () => {}
+
+    return (
+        <View style={styles.container}>
+            <View  style={styles.textInput}>
+                <TextInput style={{marginHorizontal: 5}} multiline placeholder='New Post' onChangeText={text => setPost(text)} value={post}/>
+            </View>
+            <View style={styles.postBtnContainer}>
+                <TouchableOpacity style={styles.postBtn} onPress={handleCreatePost}>
+                    <Text>Post</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
 }
 
-export default newPost
+export default NewPost
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        alignSelf: 'center',
+        borderRadius: '15%',
+        width: '90%',
+        backgroundColor: COLORS.fg1,
+    },
+    textInput: {
+        marginTop: 10,
+        width: '95%',
+        height: "50%",
+        backgroundColor: COLORS.bg2,
+        borderRadius: '10%',
+    },
+    postBtnContainer: {
+        flex: 1,
+        width: '95%',
+        marginTop: 10,
+        alignItems: "flex-end",
+    },
+    postBtn: {
+        backgroundColor: COLORS.bg1,
+        paddingHorizontal: 10,
+        paddingVertical: 3,
+        borderRadius: '10%',
+    }
+
+})
